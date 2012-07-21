805a4d0e6d9829c14c661ed0fb74c9b261d08179
Adam Argyle <atom@argyleink.com>
2012-07-21 00:38:53 -0700

    removing android scroll component

--- js/android.js
+++ js/android.js
@@ -1,11 +1,11 @@
 var ua = navigator.userAgent.toLowerCase();
 var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
 
-if (isAndroid) {
+// if (isAndroid) {
     
-    // android needs special scripts to enable overflow scrolling that feels native
-    document.write('<scr'+'ipt type="text/javascript" src="js/plugins/scrollview.js" ></scr'+'ipt>');
-    document.write('<scr'+'ipt type="text/javascript" src="js/plugins/jquery.mobile.scrollview.js" ></scr'+'ipt>');
-    document.write('<scr'+'ipt type="text/javascript" src="http://redone.org/_scrollview/include/jquery.easing.1.3.js" ></scr'+'ipt>');
+//     // android needs special scripts to enable overflow scrolling that feels native
+//     document.write('<scr'+'ipt type="text/javascript" src="js/plugins/scrollview.js" ></scr'+'ipt>');
+//     document.write('<scr'+'ipt type="text/javascript" src="js/plugins/jquery.mobile.scrollview.js" ></scr'+'ipt>');
+//     document.write('<scr'+'ipt type="text/javascript" src="http://redone.org/_scrollview/include/jquery.easing.1.3.js" ></scr'+'ipt>');
 
-}
+// }
